ce988bbcb4b6ec65b99d902e17ee7fc9a1838fbc
shivam4490 <csg9152@gmail.com>
2021-04-23 13:04:59 +0530

    monster attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const attackValue = 10;
+const monsterAttackValue = 15;
 let chosenMaxLife = 100;
 
 const currentMonsterHealth = chosenMaxLife;
@@ -9,5 +10,16 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
    const damage = dealMonsterDamage(attackValue);
    currentMonsterHealth -= damage;
+
+   const playerDamage = dealPlayerDamage(monsterAttackValue);
+   currentPlayerHealth -= playerDamage;
+
+   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+      alert('You won!');
+   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+      alert('You Lost!');
+   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+      alert('Draw!');
+   }
 }
 attackBtn.addEventListener('click', attackHandler);
